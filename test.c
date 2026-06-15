5115da5ce339b6840328ccb2e4b003ee1eca8405
Tali Auster <me@tali.network>
2026-06-15 15:03:49 +0200

    lol
    Signed-off-by: Tali Auster <me@tali.network>

--- test.c
+++ test.c
@@ -1,15 +1,18 @@
+#include <stdio.h>
 #include <sys/personality.h>
 
-int main(void)
-{
-	int old_personality = personality(0xffffffff);
-	if (old_personality == -1) {
-		printf("can't get personality\n");
-		return 1;
-	}
+int main(void) {
+  int old_personality = personality(0xffffffff);
+  if (old_personality == -1) {
+    printf("can't get personality\n");
+    return 1;
+  }
 
-	if (personality(old_personality | ADDR_NO_RANDOMIZE) == -1) {
-		printf("can't set personality\n");
-		return 1;
-	}
+  if (personality(old_personality | ADDR_NO_RANDOMIZE) == -1) {
+    printf("can't set personality\n");
+    return 1;
+  }
+
+  printf("all is well :(\n");
+  return 0;
 }
